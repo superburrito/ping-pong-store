f918b54d146a091bf9409d6c580706eda0da4047
superburrito <chuayaohui@gmail.com>
2016-07-25 18:50:52 -0400

    day1 done2

--- server/db/index.js
+++ server/db/index.js
@@ -8,13 +8,10 @@ var Review = require('./models/review');
 
 //Relationships
 User.hasMany(Order);
-Order.belongsTo(User);
 Order.belongsToMany(Product, { through: 'orderproductpivot' });
 Product.belongsToMany(Order, { through: 'orderproductpivot' });
 Product.hasMany(Review);
-Review.belongsTo(Product);
 User.hasMany(Review);
-Review.belongsTo(User);
 
 
 module.exports = {
