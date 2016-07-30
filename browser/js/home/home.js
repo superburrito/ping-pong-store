f75502f61c233901e7f6ea8dfe46589c3a8d6f74
John Henry <John.J.Henry4@gmail.com>
2016-07-30 14:44:27 -0400

    angular filter working

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -3,6 +3,25 @@ app.config(function ($stateProvider) {
       url: '/',
       templateUrl: 'js/home/home.html',
    		controller: function($scope, homepage){
+        $scope.categories = ['paddle','ball','case',
+                    'table','robot'];
+        $scope.selectedCategory = '';
+
+        $scope.orderOptions = ['price', 'rating'];
+        $scope.orderOption = '';
+        $scope.finalOrderOption = $scope.ascDesc + $scope.orderOption;
+
+        $scope.ascDescOptions = ['ascending', 'descending']
+        $scope.selectedAscDesc = 'descending';
+        $scope.ascDesc = function (){
+          if ($scope.selectedAscDesc === 'ascending'){
+            return false;
+
+          }
+          else return true;
+        }
+
+
    			homepage.getAllProducts().then(function(allProducts){
    				$scope.products = allProducts
    			})
