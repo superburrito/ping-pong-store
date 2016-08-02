7e2e541cafb5b6b65592305a4dec61c7adc2daa9
John Henry <John.J.Henry4@gmail.com>
2016-08-02 15:13:53 -0400

    Oauth working

--- server/env/development.js
+++ server/env/development.js
@@ -4,17 +4,17 @@ module.exports = {
   "TWITTER": {
     "consumerKey": "tjRt8Cuk7reHV6CIU9YT9zQki",
     "consumerSecret": "el7z2EswwOfUWJId0TNQIvpspGugmVTswCZ9EwXVHXcYp0LCJC",
-    "callbackUrl": "http://www.localhost:1337/auth/twitter/callback"
+    "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
   },
   "FACEBOOK": {
     "clientID": "022a9ed22485b866ec45b04675c6f421",
     "clientSecret": "6780027c94558965aa6611887c1ca58b",
-    "callbackURL": "http://localhost:1337/auth/facebook/callback"
+    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "386877686226-rvhgr7rpobqnscmd9grprr9sbg16h6q0.apps.googleusercontent.com",
     "clientSecret": "ba3nDL_YsL8kMnDO_QLWLVPl",
-    "callbackURL": "http://localhost:1337/auth/google/callback"
+    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
   },
   "LOGGING": true
 };
